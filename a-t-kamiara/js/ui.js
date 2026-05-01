a08ca0aaa7529000d812d161e2ac1549c56fc222
vVD4nt3xVv <richar_gb11@hotmail.com>
2026-04-30 23:36:48 -0500

    Update ui.js

--- a-t-kamiara/js/ui.js
+++ a-t-kamiara/js/ui.js
@@ -1,1 +1,14 @@
+function render(lista){
+  let html="";
 
+  lista.forEach(p=>{
+    html+=`
+    <div class="card" onclick="verProducto('${p.id}')">
+      <img src="${p.imagen}">
+      <h3>${p.producto}</h3>
+      <p>S/ ${p.unidad}</p>
+    </div>`;
+  });
+
+  productos.innerHTML=html;
+}
