1bc1b4dcbb05e8d293bb79be765ec630f6b77164
vVD4nt3xVv <richar_gb11@hotmail.com>
2026-04-30 23:36:08 -0500

    Update core.js

--- a-t-kamiara/js/core.js
+++ a-t-kamiara/js/core.js
@@ -1,1 +1,25 @@
+const usuariosSistema = {
+  luis:"123",
+  katy:"123",
+  katherine:"123",
+  richard:"123",
+  dante:"admin123"
+};
 
+const admins=["dante"];
+
+function guardarUsuario(){
+  const user=usuario.value.toLowerCase();
+  const pass=password.value;
+
+  if(!usuariosSistema[user]) return errorLogin.innerText="Usuario no registrado";
+  if(usuariosSistema[user]!==pass) return errorLogin.innerText="Contraseña incorrecta";
+
+  localStorage.setItem("usuario",user);
+  location.reload();
+}
+
+function cerrarSesion(){
+  localStorage.removeItem("usuario");
+  location.reload();
+}
